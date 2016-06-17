59c2af3804934596cb51262f424b613c737a104e
Sam Jeston <sam.jeston@gmail.com>
2016-06-17 21:25:04 +1000

    Added test suite and wrote melody rhythm generator

---
+++ test/lib/melody-rhythm.spec.js
@@ -0,0 +1,15 @@
+let rhythmGenerator = require('../../lib/melody-rhythm').generate
+
+describe('melodic rhythm', () => {
+  it('each bar should contain 4 beats', () => {
+    let bars = rhythmGenerator()
+    let totals = _.mapValues(bars, (bar) => {
+      return bar.reduce((prev, current) => current + prev, 0)
+    })
+
+    totals = _.values(totals)
+    _.each(totals, (total) => {
+      expect(total).to.eql(4)
+    })
+  })
+})
---
+++ test/test-config.js
@@ -0,0 +1,6 @@
+process.env.NODE_ENV = 'test'
+var chai = require('chai')
+
+// Test globals... yes, globals. Testing should be easy!
+expect = chai.expect
+_ = require('lodash')
